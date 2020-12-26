f4611a69d6785590e561984a3093f1696d45f0e1
Thony Hermawan <thony.her@gmail.com>
2020-12-26 13:25:23 +0700

    Android icon launcher

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quran Kita',
+      title: 'Quran ID',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => HomeScreen(title: 'Quran Kita'),
+        '/': (context) => HomeScreen(title: 'Quran ID'),
         '/detail': (context) => DetailScreen()
       },
     );
